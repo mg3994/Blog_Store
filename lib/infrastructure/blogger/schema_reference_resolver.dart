689bd2282a96e52e6923c97687f1baad41d91fb0
mg3994 <manishgautammg7@yahoo.com>
2026-08-27 08:21:06 +0530

    first commit

--- lib/infrastructure/blogger/schema_reference_resolver.dart
+++ lib/infrastructure/blogger/schema_reference_resolver.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 
-import '../../features/catalog/domain/entities/schema_reference.dart';
+import '../../features/catalog/domain/services/schema_reference.dart';
 
 abstract interface class SchemaDocumentFetcher {
   Future<Map<String, dynamic>?> fetch(SchemaReference reference);
