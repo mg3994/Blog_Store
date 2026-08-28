fe0dd8dc58d6b4e20f655cd9416e98de89a17933
mg3994 <manishgautammg7@yahoo.com>
2026-08-28 08:21:57 +0530

    fix sw web commit

--- web/firebase_messaging_sw.dart
+++ web/firebase_messaging_sw.dart
@@ -6,13 +6,39 @@ import 'package:blogstore/firebase_web_config.dart';
 external void importScripts(JSString script1, JSString script2);
 
 @JS('firebase.initializeApp')
-external void initializeApp(JSObject options);
-
-// @JS('firebase.messaging')
-// external JSObject messaging();
+external JSObject initializeApp(JSObject options);
 
 @JS('firebase.messaging')
 external FirebaseMessaging messaging();
+
+@JS('firebase.messaging.isSupported')
+external JSPromise<JSBoolean> isSupported();
+
+@JS('console.log')
+external void consoleLog(JSAny? value);
+
+@JS('self')
+external ServiceWorkerGlobalScope get self;
+
+@JS()
+@staticInterop
+class ServiceWorkerGlobalScope {}
+
+extension ServiceWorkerGlobalScopeExtension on ServiceWorkerGlobalScope {
+  external ServiceWorkerRegistration get registration;
+
+  external JSAny? addEventListener(JSString type, JSFunction listener);
+
+  external JSAny? get clients;
+}
+
+@JS()
+@staticInterop
+class ServiceWorkerRegistration {}
+
+extension ServiceWorkerRegistrationExtension on ServiceWorkerRegistration {
+  external JSPromise<JSAny?> showNotification(JSString title, JSObject options);
+}
 
 @JS()
 @staticInterop
@@ -20,35 +46,146 @@ class FirebaseMessaging {}
 
 extension FirebaseMessagingExtension on FirebaseMessaging {
   external void onBackgroundMessage(JSFunction callback);
+
+  external void setDeliveryMetricsExportedToBigQuery(bool enabled);
 }
 
-@JS('console.log')
-external void consoleLog(JSAny? value);
 void main() {
+  //
+  // Firebase JS SDK
+  //
   importScripts(
     'https://www.gstatic.com/firebasejs/10.7.0/firebase-app-compat.js'.toJS,
     'https://www.gstatic.com/firebasejs/10.7.0/firebase-messaging-compat.js'
         .toJS,
   );
 
+  //
+  // Service worker install
+  //
+  self.addEventListener(
+    'install'.toJS,
+    ((JSObject event) {
+      consoleLog(self);
+      consoleLog(event);
+    }).toJS,
+  );
+
+  //
+  // Notification click
+  //
+  self.addEventListener(
+    'notificationclick'.toJS,
+    ((JSObject event) {
+      final notification = event.getProperty<JSObject?>('notification'.toJS);
+
+      notification?.callMethod<void>('close'.toJS);
+
+      final clients = self.getProperty<JSObject?>('clients'.toJS);
+
+      if (clients == null) {
+        return;
+      }
+
+      final matchAll = clients.callMethod<JSPromise<JSArray>>(
+        'matchAll'.toJS,
+        {'type': 'window'.toJS, 'includeUncontrolled': true.toJS}.jsify(),
+      );
+
+      event.callMethod<void>('waitUntil'.toJS, matchAll);
+
+      matchAll.toDart.then((clientList) {
+        for (final client in clientList.toDart) {
+          final focused = client.getProperty<JSBoolean?>('focused'.toJS);
+
+          if (focused?.toDart != true) {
+            client.callMethod<JSPromise<JSAny?>>('focus'.toJS);
+            break;
+          }
+        }
+      });
+    }).toJS,
+  );
+
+  //
+  // Firebase configuration
+  //
   final options =
-      {
+      <String, JSAny?>{
             'apiKey': FirebaseWebConfig.apiKey,
             'appId': FirebaseWebConfig.appId,
             'messagingSenderId': FirebaseWebConfig.messagingSenderId,
             'projectId': FirebaseWebConfig.projectId,
             'authDomain': FirebaseWebConfig.authDomain,
+            'databaseURL': FirebaseWebConfig.databaseURL,
             'storageBucket': FirebaseWebConfig.storageBucket,
             'measurementId': FirebaseWebConfig.measurementId,
           }.jsify()
           as JSObject;
 
-  initializeApp(options);
-  final messagingInstance = messaging();
+  final app = initializeApp(options);
 
-  messagingInstance.onBackgroundMessage(
-    ((JSAny? message) {
-      consoleLog(message);
-    }).toJS,
-  );
+  //
+  // Check browser support
+  //
+  isSupported().toDart.then((supported) {
+    if (!supported) {
+      return;
+    }
+
+    //
+    // Firebase Messaging
+    //
+    final messagingInstance = messaging();
+
+    //
+    // Equivalent to:
+    //
+    // experimentalSetDeliveryMetricsExportedToBigQueryEnabled(
+    //   messaging,
+    //   true,
+    // );
+    //
+    messagingInstance.setDeliveryMetricsExportedToBigQuery(true);
+
+    //
+    // Background message
+    //
+    messagingInstance.onBackgroundMessage(
+      ((JSAny? rawMessage) {
+        if (rawMessage == null) {
+          return;
+        }
+
+        final message = rawMessage as JSObject;
+
+        final notification = message.getProperty<JSObject?>(
+          'notification'.toJS,
+        );
+
+        if (notification == null) {
+          return;
+        }
+
+        final title = notification.getProperty<JSString?>('title'.toJS);
+
+        if (title == null || title.toDart.isEmpty) {
+          return;
+        }
+
+        final body = notification.getProperty<JSString?>('body'.toJS);
+
+        final image = notification.getProperty<JSString?>('image'.toJS);
+
+        final icon = image?.toDart.isNotEmpty == true
+            ? image!
+            : '/icons/Icon-192.png'.toJS
+
+        final notificationOptions =
+            <String, JSAny?>{'body': body, 'icon': icon}.jsify() as JSObject;
+
+        self.registration.showNotification(title, notificationOptions);
+      }).toJS,
+    );
+  });
 }
